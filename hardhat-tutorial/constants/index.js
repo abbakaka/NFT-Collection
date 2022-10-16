4d50daf2d497ee41c4634e3be77b3e605c4a17b7
Bakaka <yawudima2016@gmail.com>
2022-10-15 17:16:55 -0700

    NFT

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x1CFbCfc18C484464De0085CFF923EC68772f7820";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
